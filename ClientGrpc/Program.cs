8a378f1bf33067d579052a478d9d3de02dc2178a
m0destru <rustamghetto@gmail.com>
2022-12-09 20:43:28 +0500

    Init commit

---
+++ ClientGrpc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using FileSystemWatcher;
+
+namespace Client;
+
+public static class Program
+{
+    private static string _path = @"C:\MySyncDir";
+    
+    private static bool IsValidFilePath(string path)
+    {
+        var invalidChars = string.Join("", Path.GetInvalidPathChars());
+        var regex = new Regex("[" + Regex.Escape(string.Join("", invalidChars)) + "]");
+
+        return !regex.IsMatch(path);
+    }
+    
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("В качестве аргумента необходимо передать путь до папки, которую необходимо синхронизовать");
+            Directory.CreateDirectory(_path);
+        }
+        else if (!Directory.Exists(args[0]))
+        {
+            _path = IsValidFilePath(args[0]) ? args[0] : _path;
+            Directory.CreateDirectory(_path);
+        }
+
+        var fsWorker = new FileSystemWorker(_path);
+        Console.WriteLine("Нажмите любую клавишу, чтобы завершить работу клиента");
+        Console.ReadKey();
+    }
+}
---
+++ FileSystemWatcher/FileSystemWorker.cs
@@ -0,0 +1,73 @@
+﻿namespace FileSystemWatcher;
+
+using System;
+using System.IO;
+
+
+public class FileSystemWorker
+{
+    private readonly FileSystemWatcher _watcher;
+
+    public FileSystemWorker(string path)
+    {
+        _watcher = new FileSystemWatcher(path);
+        _watcher.NotifyFilter = NotifyFilters.Attributes
+                                | NotifyFilters.CreationTime
+                                | NotifyFilters.DirectoryName
+                                | NotifyFilters.FileName
+                                | NotifyFilters.LastAccess
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.Security
+                                | NotifyFilters.Size;
+
+        _watcher.Changed += OnChanged;
+        _watcher.Created += OnCreated;
+        _watcher.Deleted += OnDeleted;
+        _watcher.Renamed += OnRenamed;
+        _watcher.Error += OnError;
+
+        _watcher.IncludeSubdirectories = true;
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    private static void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        if (e.ChangeType != WatcherChangeTypes.Changed || Directory.Exists(e.FullPath))
+        {
+            return;
+        }
+
+        Console.WriteLine($"Изменено содержимое: {e.FullPath}\n");
+    }
+
+    private static void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        string value = $"Создано: {e.FullPath}\n";
+        Console.WriteLine(value);
+    }
+
+    private static void OnDeleted(object sender, FileSystemEventArgs e) =>
+        Console.WriteLine($"Удалено: {e.FullPath}\n");
+
+    private static void OnRenamed(object sender, RenamedEventArgs e)
+    {
+        Console.WriteLine("Переименование:");
+        Console.WriteLine($"    Было: {e.OldFullPath}");
+        Console.WriteLine($"    Стало: {e.FullPath}\n");
+    }
+
+    private static void OnError(object sender, ErrorEventArgs e) =>
+        PrintException(e.GetException());
+
+    private static void PrintException(Exception? ex)
+    {
+        if (ex != null)
+        {
+            Console.WriteLine($"Message: {ex.Message}");
+            Console.WriteLine("Stacktrace:");
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine();
+            PrintException(ex.InnerException);
+        }
+    }
+}
