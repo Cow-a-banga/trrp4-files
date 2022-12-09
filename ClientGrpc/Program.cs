3c3958bbf5f3a94d501f991ca431655caa6713f1
Dmitry <chiruhind@gmail.com>
2022-12-09 21:20:45 +0500

    actually move to .net7

--- ClientGrpc/Program.cs
+++ ClientGrpc/Program.cs
@@ -1,35 +1,38 @@
-﻿using System.Text.RegularExpressions;
-using FileSystemWatcher;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using FileSystemWork;
 
-namespace Client;
-
-public static class Program
+namespace Client
 {
-    private static string _path = @"C:\MySyncDir";
-    
-    private static bool IsValidFilePath(string path)
+    public static class Program
     {
-        var invalidChars = string.Join("", Path.GetInvalidPathChars());
-        var regex = new Regex("[" + Regex.Escape(string.Join("", invalidChars)) + "]");
-
-        return !regex.IsMatch(path);
-    }
+        private static string _path = @"C:\MySyncDir";
     
-    public static void Main(string[] args)
-    {
-        if (args.Length == 0)
+        private static bool IsValidFilePath(string path)
         {
-            Console.WriteLine("В качестве аргумента необходимо передать путь до папки, которую необходимо синхронизовать");
-            Directory.CreateDirectory(_path);
-        }
-        else if (!Directory.Exists(args[0]))
-        {
-            _path = IsValidFilePath(args[0]) ? args[0] : _path;
-            Directory.CreateDirectory(_path);
-        }
+            var invalidChars = string.Join("", Path.GetInvalidPathChars());
+            var regex = new Regex("[" + Regex.Escape(string.Join("", invalidChars)) + "]");
 
-        var fsWorker = new FileSystemWorker(_path);
-        Console.WriteLine("Нажмите любую клавишу, чтобы завершить работу клиента");
-        Console.ReadKey();
+            return !regex.IsMatch(path);
+        }
+    
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("В качестве аргумента необходимо передать путь до папки, которую необходимо синхронизовать");
+                Directory.CreateDirectory(_path);
+            }
+            else if (!Directory.Exists(args[0]))
+            {
+                _path = IsValidFilePath(args[0]) ? args[0] : _path;
+                Directory.CreateDirectory(_path);
+            }
+
+            var fsWorker = new FileSystemWorker(_path);
+            Console.WriteLine("Нажмите любую клавишу, чтобы завершить работу клиента");
+            Console.ReadKey();
+        }
     }
 }
