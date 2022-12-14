ff8d15244c0b294e9572c8ae7e697edd7445b853
Rustam <valrusrav@gmail.com>
2022-12-14 19:18:44 +0500

    remove Test

--- FileServer/MessageQueue.cs
+++ FileServer/MessageQueue.cs
@@ -14,8 +14,9 @@ namespace Client
         private string _username;
         private string _password;
         private EventHandler<BasicDeliverEventArgs> _handler;
-        
-        public MessageQueue(string hostName, string queueName, string username, string password, EventHandler<BasicDeliverEventArgs> handler)
+
+        public MessageQueue(string hostName, string queueName, string username, string password,
+            EventHandler<BasicDeliverEventArgs> handler)
         {
             _hostName = hostName;
             _queueName = queueName;
@@ -26,9 +27,9 @@ namespace Client
 
         public void Start()
         {
-            var factory = new ConnectionFactory { HostName = _hostName, UserName = _username, Password = _password};
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            var factory = new ConnectionFactory { HostName = _hostName, UserName = _username, Password = _password };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
                 channel.QueueDeclare(
                     queue: _queueName,
@@ -39,50 +40,14 @@ namespace Client
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += _handler;
-                
+
                 channel.BasicConsume(
                     queue: _queueName,
                     autoAck: true,
                     consumer: consumer);
-                
+
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
-            }
-        }
-
-        public void Test()
-        {
-            var factory = new ConnectionFactory { HostName = _hostName, UserName = _username, Password = _password};
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(
-                    queue: _queueName,
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
-
-                var id = Guid.NewGuid().ToString();
-                
-                var message1 = new Message("/", MsgType.CreateDisk)
-                {
-                    Id = id
-                };
-                
-                var message2 = new Message("NewDir", MsgType.CreateDirectory)
-                {
-                    Id = id
-                };
-
-                var json1 = JsonConvert.SerializeObject(message1);
-                var json2 = JsonConvert.SerializeObject(message2);
-
-                var body1 = Encoding.UTF8.GetBytes(json1);
-                var body2 = Encoding.UTF8.GetBytes(json2);
-
-                channel.BasicPublish("", _queueName, null, body1);
-                channel.BasicPublish("", _queueName, null, body2);
             }
         }
     }
